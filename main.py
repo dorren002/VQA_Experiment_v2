45ad37995a8be063913877355d829651a11c17aa
dorren <dorren1998@outlook.com>
2021-05-12 11:34:27 +0800

    update icarl inter in main

--- main.py
+++ main.py
@@ -503,6 +503,8 @@ def main():
 
         if args.offline:
             training_loop(config, net, train_data, val_data, optimizer, criterion, config.expt_dir, net_running, start_epoch)
+        if args.icarl:
+            train_icarl_manner(config, net, train_data, val_data, optimizer, criterion, config.expt_idr, net_running, start_epoch)
         elif config.max_epochs>0:
             train_base_init(config, net, train_data, val_data, optimizer, criterion, args.expt_name, net_running)
 
